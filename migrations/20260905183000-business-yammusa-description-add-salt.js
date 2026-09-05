529cc77a45547ab779ed19fe14e798f2e8fbd159
8790fahad <8790fahadado@gmail.com>
2026-09-05 18:09:11 +0100

    changes

---
+++ migrations/20260905183000-business-yammusa-description-add-salt.js
@@ -0,0 +1,53 @@
+"use strict";
+
+/**
+ * Add Salt to the YAMMUSA letterhead description line.
+ *
+ * Business: 094c6e1e-dd07-48c4-a344-6e9d58cd7861
+ */
+const BUSINESS_ID = "094c6e1e-dd07-48c4-a344-6e9d58cd7861";
+const NEW_DESCRIPTION =
+  "Dealer in all types Rice, Sugar, Pasta, Flour and Salt etc.";
+const OLD_DESCRIPTION =
+  "Dealer in all types Rice, Sugar, Pasta and Flour etc.";
+
+module.exports = {
+  async up(queryInterface) {
+    await queryInterface.sequelize.query(
+      `
+      UPDATE business
+      SET description = :description
+      WHERE id = :id
+         OR UPPER(business_name) LIKE '%YAMMUSA%'
+         OR UPPER(business_name) LIKE '%YAMUSA%'
+         OR UPPER(business_name) LIKE '%ALI MUHAMMAD%'
+         OR description = :oldDescription
+      `,
+      {
+        replacements: {
+          id: BUSINESS_ID,
+          description: NEW_DESCRIPTION,
+          oldDescription: OLD_DESCRIPTION,
+        },
+      },
+    );
+  },
+
+  async down(queryInterface) {
+    await queryInterface.sequelize.query(
+      `
+      UPDATE business
+      SET description = :description
+      WHERE id = :id
+         OR description = :newDescription
+      `,
+      {
+        replacements: {
+          id: BUSINESS_ID,
+          description: OLD_DESCRIPTION,
+          newDescription: NEW_DESCRIPTION,
+        },
+      },
+    );
+  },
+};
